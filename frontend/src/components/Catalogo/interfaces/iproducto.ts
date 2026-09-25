659866e306ec24d7f2d8bb423782d166a8c3ab87
CarlosFernandoPatinoGarcia <carlos.patinogarcia9447@upse.edu.ec>
2026-09-24 21:52:02 -0500

    feat(frontend): datos mock de productos

--- frontend/src/components/Catalogo/interfaces/iproducto.ts
+++ frontend/src/components/Catalogo/interfaces/iproducto.ts
@@ -1,6 +1,9 @@
 export interface IProducto {
   id: number;
   nombre: string;
+  descripcion: string;
   precio: number;
-  img: string;
+  categoria: string;
+  img: string;        // Imagen principal (portada)
+  galeria: string[];  // Imágenes adicionales para la galería/lightbox
 }
